8b647d1747b777b6585d984b16377623b0c9de84
febbraiod <donedesign1@gmail.com>
2016-09-21 14:01:42 -0400

    gradient effect on background image

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@ $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
     if (scroll >= 120) {
+      $("#down_arrow").hide();
       $("#nav_container").addClass("top_nav");
       $("#nav_container").removeClass("bottom_nav");
     } else {
       $("#nav_container").addClass("bottom_nav");
       $("#nav_container").removeClass("top_nav");
+      $("#down_arrow").show();
     }
 });
